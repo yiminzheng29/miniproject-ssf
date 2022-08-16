dcbadd595c76835bec44e20f61d9433d7e3f530c
yiminzheng29 <yiminzheng29@gmail.com>
2022-08-16 10:33:15 +0800

    added AppConfig and updated dependencies

---
+++ src/test/java/vttp/ssf/SpotifyApp/SpotifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.ssf.SpotifyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
