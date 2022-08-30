e811d03839c530bbdd759e065060990b1b1f31e6
yiminzheng29 <yiminzheng29@gmail.com>
2022-08-30 17:52:29 +0800

    Updated index page to include bootstrap CSS

--- src/main/java/vttp/ssf/SpotifyApp/controllers/PlayerRestController.java
+++ src/main/java/vttp/ssf/SpotifyApp/controllers/PlayerRestController.java
@@ -1,13 +1,11 @@
 package vttp.ssf.SpotifyApp.controllers;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
